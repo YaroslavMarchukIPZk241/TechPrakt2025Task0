e4cc484c94aa4bcf638498923793645c45f1c4b0
ipzk241_myav <ipzk241_myav@student.ztu.edu.ua>
2025-01-24 17:33:43 +0200

    my first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {      
+            Console.WriteLine(int.Parse(Console.ReadLine()) + int.Parse(Console.ReadLine()));        
+    }
+}
